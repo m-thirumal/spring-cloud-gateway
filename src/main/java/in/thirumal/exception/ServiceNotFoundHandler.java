2eb07e3bd52925d47318507e672a35a499a27e20
Thirumal <m.thirumal@hotmail.com>
2020-03-07 14:06:35 +0530

    Exception

---
+++ src/main/java/in/thirumal/exception/ServiceNotFoundHandler.java
@@ -0,0 +1,24 @@
+package in.thirumal.exception;
+
+import org.springframework.cloud.gateway.support.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * 
+ * @author thirumal
+ *
+ */
+@ControllerAdvice
+public class ServiceNotFoundHandler {
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    public String handleUnavailableService(NotFoundException e) {
+    	System.out.println("ji");
+        return "service unavailable";
+    }
+}
