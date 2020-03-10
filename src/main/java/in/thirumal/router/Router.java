7609130156a6fcda980317127ec4d5eb31ee8ee3
Thirumal <m.thirumal@hotmail.com>
2020-03-10 10:51:45 +0530

    updated

--- src/main/java/in/thirumal/router/Router.java
+++ src/main/java/in/thirumal/router/Router.java
@@ -3,8 +3,6 @@
  */
 package in.thirumal.router;
 
-import java.util.Set;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +12,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import in.thirumal.throttle.AuthorizationKeyResolver;
 import in.thirumal.throttle.RemoteAddressKeyResolver;
@@ -37,13 +35,13 @@ public class Router {
 	@Bean
 	public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
 		logger.debug(this.getClass().getSimpleName() + ": " + Thread.currentThread().getStackTrace()[1].getMethodName());
-		final String lb;
+		final String lb = "lb://eureka-client-1";
 		final String defaultLb ="lb://go-to-hell";
-		if (Set.of("DEV").contains(activeProfile)) {
-			lb = "http://localhost:8082";
-		} else {
-			lb = "lb://eureka-client-2";
-		}
+//		if (Set.of("DEV").contains(activeProfile)) {
+//			lb = "lb://eureka-client-1";
+//		} else {
+//			lb = "lb://eureka-client-1";
+//		}
 		return routeLocatorBuilder.routes()
 			/*	.route("client2", r -> r.path("/api/oauth/token")
 						.filters(f -> f.filter((exchange, chain) -> {
@@ -53,7 +51,7 @@ public class Router {
 									.build();
 							return chain.filter(exchange.mutate().request(request).build());
 						})).uri(lb))*/
-				.route("client2", r -> r.path("/**")
+				.route("client1", r -> r.path("/1/**")
 					.filters(f -> 
 						f.requestRateLimiter()
 								.rateLimiter(RedisRateLimiter.class, c -> c.setBurstCapacity(10).setReplenishRate(4))
@@ -62,7 +60,7 @@ public class Router {
 								.configure(c -> c.setKeyResolver(authorizationKeyResolver)
 										.setKeyResolver(remoteAddressKeyResolver)).retry(3)
 								.secureHeaders()
-								.addResponseHeader("app", "client2")
+								.addResponseHeader("app", "client1")
 								//.addResponseHeader("response-time", LocalDateTime.now().toString())
 								.hystrix(h -> h.setName("gateway Fallback").setFallbackUri("forward:/default-gateway"))
 						) // add response header
@@ -70,13 +68,13 @@ public class Router {
 						// .route(r -> r.header("X-Request-Id", "\\d+")
 						.uri(lb))
 				.route("default", r -> r.path("/**").filters(f -> f//.rewritePath("/*", "/default-icms")
-						.hystrix(h -> h.setName("gateway Fallback").setFallbackUri("forward:/default-gateway")))
+						.hystrix(h -> h.setName("gateway Fallback").setFallbackUri("forward:/fallback/default-gateway")))
 						.uri(defaultLb))
 				.build();
 				
 	}
 	
-	@RequestMapping("/default-gateway")
+	@GetMapping("/fallback/default-gateway")
 	public String defaultGateway() {
 		return "Message from default gateway";
 	}
